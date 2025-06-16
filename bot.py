5524b8cdf8a9f7acbb4ddef24ca68b2b654bdd5b
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2025-06-16 22:36:40 +0000

    first commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="b!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+
+bot.run(TOKEN)
